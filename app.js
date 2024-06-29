fbba7589dca743d8c2e924cced908b2da6f6450b
Ömer Yılmaz <omeryilmaz.jr@gmail.com>
2024-06-29 18:23:26 +0300

    update

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ const app = express();
 require('dotenv').config();
 require("./database/db")();
 
-
 const userRouter = require("./routes/user");
 const taskRouter = require("./routes/task");
 
 app.use(express.json());
-
 app.use(userRouter);
 app.use(taskRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listeining on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
